cc9e7b71e05dcf5658b1e485a3da946c19bdf043
simon clement <sim.set@wanadoo.fr>
2026-07-01 15:42:41 +0200

    Fix session production Render 502

--- app.js
+++ app.js
@@ -47,10 +47,9 @@ app.use(session({
     secret: process.env.JWT_SECRET || 'secret',
     resave: false,
     saveUninitialized: false,
-    proxy: true,
     cookie: {
-        secure: true,
-        sameSite: 'none'
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
     }
 }));
 
